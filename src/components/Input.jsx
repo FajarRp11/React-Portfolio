97be8a84757f7a43a3a02075e53a5331dce5b831
Fajar Rahyudi Putra <fajarrahyu1103@gmail.com>
2024-08-03 20:01:53 +0700

    DONE

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ const Input = (props) => {
       type={type}
       name={name}
       id={id}
-      className="w-full px-4 py-2 rounded outline-none bg-gray-900/65 placeholder:text-gray-50"
+      className="w-full px-4 py-2 rounded outline-none text-gray-50 bg-gray-900/65 placeholder:text-gray-50/50"
       placeholder={placeholder}
       required
     />
