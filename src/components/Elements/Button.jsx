738fabf49439396b30cdcd5e07b0809896fdb1f5
Fajar Rahyudi Putra <fajarrahyu1103@gmail.com>
2024-08-03 20:48:58 +0700

    DONE

--- src/components/Elements/Button.jsx
+++ src/components/Elements/Button.jsx
@@ -2,11 +2,12 @@ const Button = (props) => {
   const {
     children,
     variant = "bg-blue-600 text-gray-50",
+    width = "w-fit",
     type = "button",
   } = props;
   return (
     <div
-      className={`flex items-center gap-2 px-6 py-2 border border-blue-600 tracking-wider cursor-pointer font-medium ${variant} rounded-full w-fit`}
+      className={`flex items-center justify-center gap-2 px-6 py-2 border border-blue-600 tracking-wider cursor-pointer font-medium ${variant} ${width} rounded-full`}
       type={type}
     >
       {children}
